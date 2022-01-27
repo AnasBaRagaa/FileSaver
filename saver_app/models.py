559c83327a04bab4cf72e966fda696ed09e636e5
AnasBaRagaa <b00075797@aus.edu>
2022-01-27 09:50:11 +0300

    Data model and corresponding database table created

--- saver_app/models.py
+++ saver_app/models.py
@@ -2,3 +2,11 @@ from django.db import models
 
 # Create your models here.
 from django.contrib.auth.models import User
+
+
+class Data(models.Model):
+    title = models.CharField(max_length=100)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    file = models.FileField(upload_to='uploads')
+    description= models.CharField(max_length=500, null=True, blank=True )
+
